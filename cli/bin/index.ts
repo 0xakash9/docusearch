67f28bbc7d8815ed179be593feb297714006fbd3
0xakash9 <devakash256@gmail.com>
2024-02-16 22:47:49 +0530

    cmd

--- cli/bin/index.ts
+++ cli/bin/index.ts
@@ -1,7 +1,31 @@
-import { intro } from "@clack/prompts";
+#! /usr/bin/env node
+import clack from "@clack/prompts";
 
 async function main() {
-  intro("select starter");
+  clack.intro("select starter");
+
+  const packagemanager = await clack.select({
+    message: "Select Package manager",
+    options: [
+      { value: "pnpm", label: "pnpm" },
+      { value: "npm", label: "npm" },
+      { value: "yarn", label: "yarn" },
+      { value: "bun", label: "bun" },
+    ],
+  });
+
+  const selectdocsframework = await clack.select({
+    message: "Select docs framework",
+    options: [
+      { value: "vocs", label: "vocs" },
+      { value: "nextra", label: "nextra" },
+      { value: "docusaurus", label: "docusaurus" },
+    ],
+  });
+  console.log("root", process.cwd());
+
+  console.log("packageManager", packagemanager);
+  console.log("selectdocsframework", selectdocsframework);
 }
 
 main().catch(console.error);
